74acca4eeea3b3995958d7ecd07144f4e797721a
lsandaruwanjayas <fgod406@gmail.com>
2024-11-28 17:32:43 +0530

    Sort is working

--- src/script/main.js
+++ src/script/main.js
@@ -6,22 +6,52 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
-  updateMinToMaxUI(inputValues);
-  updateMaxToMinUI(inputValues);
+ 
+  updateMinToMaxUI(bubbleSortMinToMax(inputValues))
+  updateMaxToMinUI(bubbleSortMaxToMin(inputValues))
 };
 
 const updateMinToMaxUI = (array = []) => {
-    array.forEach((num, i) => {
-      const outputValueNode = document.getElementById(`output-min-value-${i}`);
-      outputValueNode.innerText = num;
-    });
-}
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-min-value-${i}`);
+    outputValueNode.innerText = num;
+  });
+};
+
+const bubbleSortMinToMax = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          const temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }
+    return array;
+  };
 
 const updateMaxToMinUI = (array = []) => {
-    array.forEach((num, i) => {
-      const outputValueNode = document.getElementById(`output-max-value-${i}`);
-      outputValueNode.innerText = num;
-    });
-}
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-max-value-${i}`);
+    outputValueNode.innerText = num;
+  });
+};
+
+
+const bubbleSortMaxToMin = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - 1; j++) {
+          if (array[j] < array[j + 1]) {
+            const temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+          }
+        }
+      }
+      return array;
+  };
+
+
 
 sortButton.addEventListener("click", sortInputArray);
